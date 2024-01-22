bca8cd4c1cc00e3dcacfad172d5fa6382387310e
ris4rob <108408741+ris4rob@users.noreply.github.com>
2024-01-22 09:21:44 +0800

    Add variable to prompt for players turn and function to add turn to board

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function Game(){
+(function game(){
 
     // This is the gameboard filled with nulls //
     let gameboard = [[null, null, null], [null, null, null], [null, null, null]]
+    let playerOnesTurn = prompt('choose a space')
 
+    function playersTurn(turn){
+        
+        turn = turn.split()
+        gameboard[turn]
+        
+    }
 
-}
+    playersTurn(playerOnesTurn)
+})
